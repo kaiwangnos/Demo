3dab353813969444c85d5eaef6de22d20ce9bc43
kai <870505786@qq.com>
2017-08-27 23:22:33 +0800

    2017/08/27

--- src/main/java/com/ssm/service/impl/UserServiceImpl.java
+++ src/main/java/com/ssm/service/impl/UserServiceImpl.java
@@ -53,6 +53,6 @@ public class UserServiceImpl implements UserService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return true;
     }
 }
